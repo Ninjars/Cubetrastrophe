87ae01c79201fc7c52e9f3ad8cd1f708da069e15
jez <jeremy+github@jeremystevens.net>
2019-09-21 23:53:29 +0100

    renamed folders for jollies, tweaked a bit of the spawn logic

---
+++ Assets/Field/JitterField.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class JitterField : MonoBehaviour {
+
+    public JitterCube cubePrefab;
+    public int gridSize = 100;
+    public GameObject spherePrefab;
+    public int sphereCount = 50;
+    private JitterCube[,] cubes;
+
+    void Start() {
+        cubes = new JitterCube[gridSize, gridSize];
+
+        var gridOffset = gridSize / 2;
+        for (var x = 0; x < gridSize; x++) {
+            for (var y = 0; y < gridSize; y++) {
+                var cube = GameObject.Instantiate(cubePrefab);
+                cube.transform.position = new Vector3(x - gridOffset, 0, y);
+                cube.name = $"{x}, {y}";
+                cubes[x, y] = cube;
+            }
+        }
+
+        for (var i = 0; i < sphereCount; i++) {
+            var instance = GameObject.Instantiate(spherePrefab);
+            instance.transform.position =new Vector3(UnityEngine.Random.value, 2 + i * 0.6f, gridOffset / 3 + UnityEngine.Random.value);
+        }
+    }
+}
---
+++ Assets/FieldEcs/JitterFieldECS.cs
@@ -0,0 +1,33 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+public class JitterFieldECS : MonoBehaviour {
+
+    public GameObject cubePrefab;
+    public int gridSize = 100;
+    public GameObject spherePrefab;
+    public int sphereCount = 50;
+
+    void Start() {
+        // Create entity prefab from the game object hierarchy once
+        var cube = GameObjectConversionUtility.ConvertGameObjectHierarchy(cubePrefab, World.Active);
+        var sphere = GameObjectConversionUtility.ConvertGameObjectHierarchy(spherePrefab, World.Active);
+        var entityManager = World.Active.EntityManager;
+
+        var gridOffset = gridSize / 2;
+        for (var x = 0; x < gridSize; x++) {
+            for (var y = 0; y < gridSize; y++) {
+                var instance = entityManager.Instantiate(cube);
+                entityManager.SetComponentData(instance, new Translation { Value = new float3(x - gridOffset, 0, y) });
+                entityManager.AddComponentData(instance, new FieldCubeTag {});
+            }
+        }
+
+        for (var i = 0; i < sphereCount; i++) {
+            var instance = entityManager.Instantiate(sphere);
+            entityManager.SetComponentData(instance, new Translation { Value = new float3(UnityEngine.Random.value, 2 + i * 0.6f, 50 + UnityEngine.Random.value) });
+        }
+    }
+}
