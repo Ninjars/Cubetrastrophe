8c66a32169bedd589ea77d0de0acf08e6469c504
jez <jeremy+github@jeremystevens.net>
2019-09-26 00:00:05 +0100

    re-arranged TurretControlSystem for ease of reading

--- Assets/Battleground/Scripts/TurretControlSystem.cs
+++ Assets/Battleground/Scripts/TurretControlSystem.cs
@@ -8,6 +8,55 @@ using Unity.Physics;
 using Unity.Transforms;
 using UnityEngine;
 
+struct UpdateTurretJob : IJobForEach<LocalToWorld, Rotation, GunData, GunState> {
+    public EntityCommandBuffer commandBuffer;
+    public float deltaTime;
+
+    public void Execute(
+            [ReadOnly] ref LocalToWorld transform,
+            ref Rotation rotation,
+            [ReadOnly] ref GunData gun,
+            ref GunState state) {
+        rotation.Value = math.mul(math.normalizesafe(rotation.Value), quaternion.AxisAngle(math.up(), 0.01f));
+        if (state.shotsRemaining > 0) {
+            if (state.currentFireInterval > 0) {
+                state.currentFireInterval = state.currentFireInterval - deltaTime;
+            } else {
+                fireProjectile(ref transform, ref rotation, ref gun, ref state);
+            }
+        } else {
+            if (state.currentReloadInterval > 0) {
+                state.currentReloadInterval = state.currentReloadInterval - deltaTime;
+            } else {
+                state.shotsRemaining = gun.shotsPerReload;
+                state.currentReloadInterval = 0;
+                state.currentFireInterval = 0;
+            }
+        }
+    }
+
+    private void fireProjectile(ref LocalToWorld transform, ref Rotation rotation, ref GunData gun, ref GunState state) {
+        state.shotsRemaining = state.shotsRemaining - 1;
+
+        var instance = commandBuffer.Instantiate(gun.projectileEntity);
+        commandBuffer.SetComponent(instance, new Translation { Value = transform.Position + math.rotate(rotation.Value.value, gun.projectileOffset) });
+        commandBuffer.SetComponent(instance, new Rotation { Value = rotation.Value });
+        commandBuffer.SetComponent(instance, new PhysicsVelocity() {
+            Linear = math.mul(rotation.Value.value, new float3(gun.projectileVelocity, 0, 0)),
+            Angular = float3.zero
+        });
+
+        if (state.shotsRemaining <= 0) {
+            state.currentFireInterval = 0;
+            state.currentReloadInterval = gun.reloadInterval;
+            state.shotsRemaining = 0;
+
+        } else {
+            state.currentFireInterval = gun.fireInterval;
+        }
+    }
+}
+
 public class TurretControlSystem : JobComponentSystem {
     EntityQuery queryGroup;
     EndSimulationEntityCommandBufferSystem bufferSystem;
@@ -15,61 +64,11 @@ public class TurretControlSystem : JobComponentSystem {
     protected override void OnCreate() {
         // Cached access to a set of ComponentData based on a specific query
         queryGroup = GetEntityQuery(
-            ComponentType.ReadOnly<LocalToWorld>(), 
-            typeof(Rotation), 
-            ComponentType.ReadOnly<GunData>(), 
+            ComponentType.ReadOnly<LocalToWorld>(),
+            typeof(Rotation),
+            ComponentType.ReadOnly<GunData>(),
             typeof(GunState));
         bufferSystem = World.Active.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
-    }
-
-    struct UpdateTurretJob : IJobForEach<LocalToWorld, Rotation, GunData, GunState> {
-        public EntityCommandBuffer commandBuffer;
-        public float deltaTime;
-
-        public void Execute(
-                [ReadOnly] ref LocalToWorld transform, 
-                ref Rotation rotation, 
-                [ReadOnly] ref GunData gun, 
-                ref GunState state) {
-            rotation.Value = math.mul(math.normalizesafe(rotation.Value), quaternion.AxisAngle(math.up(), 0.01f));
-            if (state.shotsRemaining > 0) {
-                if (state.currentFireInterval > 0) {
-                    state.currentFireInterval = state.currentFireInterval - deltaTime;
-                } else {
-                    fireProjectile(ref transform, ref rotation, ref gun, ref state);
-                }
-            } else {
-                if (state.currentReloadInterval > 0) {
-                    state.currentReloadInterval = state.currentReloadInterval - deltaTime;
-                } else {
-                    state.shotsRemaining = gun.shotsPerReload;
-                    state.currentReloadInterval = 0;
-                    state.currentFireInterval = 0;
-                }
-            }
-        }
-
-        private void fireProjectile(ref LocalToWorld transform, ref Rotation rotation, ref GunData gun, ref GunState state) {
-            state.shotsRemaining = state.shotsRemaining - 1;
-
-            var instance = commandBuffer.Instantiate(gun.projectileEntity);
-            Debug.Log(transform.Forward);
-            commandBuffer.SetComponent(instance, new Translation { Value = transform.Position + math.rotate(rotation.Value.value, gun.projectileOffset) });
-            commandBuffer.SetComponent(instance, new Rotation { Value = rotation.Value });
-            commandBuffer.SetComponent(instance, new PhysicsVelocity() { 
-                Linear = math.mul(rotation.Value.value, new float3(gun.projectileVelocity, 0, 0)),
-                Angular = float3.zero
-            });
-
-            if (state.shotsRemaining <= 0) {
-                state.currentFireInterval = 0;
-                state.currentReloadInterval = gun.reloadInterval;
-                state.shotsRemaining = 0;
-
-            } else {
-                state.currentFireInterval = gun.fireInterval;
-            }
-        }
     }
 
     protected override JobHandle OnUpdate(JobHandle inputDependencies) {
