8e05134b193809beae6a726894e061b7c261895f
jez <jeremy+github@jeremystevens.net>
2019-09-25 23:21:09 +0100

    managed to get collision event callbacks :D

--- Assets/Battleground/Scripts/CollisionSystem.cs
+++ Assets/Battleground/Scripts/CollisionSystem.cs
@@ -1,49 +1,44 @@
-﻿// using Unity.Collections;
-// using Unity.Entities;
-// using Unity.Jobs;
-// using Unity.Physics;
-// using Unity.Physics.Systems;
-// using UnityEngine;
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Physics;
+using Unity.Physics.Systems;
+using UnityEngine;
 
-// [UpdateAfter(typeof(StepPhysicsWorld))]
-// [UpdateBefore(typeof(EndFramePhysicsSystem))]
-// public class CollisionSystem : JobComponentSystem {
+[UpdateAfter(typeof(StepPhysicsWorld))]
+[UpdateBefore(typeof(EndFramePhysicsSystem))]
+public class CollisionSystem : JobComponentSystem {
 
-//     struct CollisionJob : ICollisionEventsJob {
-//         [ReadOnly] public PhysicsWorld physicsWorld;
+    struct CollisionJob : ICollisionEventsJob {
+        [ReadOnly] public PhysicsWorld physicsWorld;
+        [ReadOnly] public NativeSlice<RigidBody> Bodies;
 
-//         public void Execute(CollisionEvent ev) {
-//             Entity a = physicsWorld.Bodies[ev.BodyIndices.BodyAIndex].Entity;
-//             Entity b = physicsWorld.Bodies[ev.BodyIndices.BodyBIndex].Entity;
-//             Debug.Log($"collision event: {ev}. Entities: {a}, {b}");
-//         }
-//     }
+        public void Execute(CollisionEvent ev) {
+            Entity a = physicsWorld.Bodies[ev.BodyIndices.BodyAIndex].Entity;
+            Entity b = physicsWorld.Bodies[ev.BodyIndices.BodyBIndex].Entity;
+            Debug.Log($"collision event: Normal {ev.Normal} Entities: {a}, {b}");
+        }
+    }
 
+    BuildPhysicsWorld buildPhysicsWorldSystem;
+    StepPhysicsWorld stepPhysicsWorld;
+    EndFramePhysicsSystem endFramePhysicsSystem;
 
-//     BuildPhysicsWorld buildPhysicsWorldSystem;
-//     StepPhysicsWorld stepPhysicsWorld;
-//     EndFramePhysicsSystem endFramePhysicsSystem;
+    protected override void OnCreate() {
+        buildPhysicsWorldSystem = World.GetOrCreateSystem<BuildPhysicsWorld>();
+        stepPhysicsWorld = World.GetOrCreateSystem<StepPhysicsWorld>();
+    }
 
-//     protected override void OnCreate() {
-//         buildPhysicsWorldSystem = World.GetOrCreateSystem<BuildPhysicsWorld>();
-//         stepPhysicsWorld = World.GetOrCreateSystem<StepPhysicsWorld>();
-//     }
+    protected override JobHandle OnUpdate(JobHandle inputDeps) {
+        SimulationCallbacks.Callback testCollisionEventCallback = (ref ISimulation simulation, ref PhysicsWorld world, JobHandle inDeps) => {
+            return new CollisionJob {
+                physicsWorld = world,
+                Bodies = world.Bodies
+            }.Schedule(simulation, ref world, inDeps);
+        };
 
-//     protected override JobHandle OnUpdate(JobHandle inputDeps) {
-//         Debug.Log($"collision system running: {Time.time}"); // this runs correctly
+        stepPhysicsWorld.EnqueueCallback(SimulationCallbacks.Phase.PostSolveJacobians, testCollisionEventCallback, inputDeps);
 
-//         inputDeps = JobHandle.CombineDependencies(inputDeps, buildPhysicsWorldSystem.FinalJobHandle);
-//         inputDeps = JobHandle.CombineDependencies(inputDeps, stepPhysicsWorld.FinalJobHandle);
-
-//         var physicsWorld = buildPhysicsWorldSystem.PhysicsWorld;
-
-//         var collisionJob = new CollisionJob {
-//             physicsWorld = physicsWorld
-//         };
-//         JobHandle collisionHandle = collisionJob.Schedule(stepPhysicsWorld.Simulation, ref physicsWorld, inputDeps);
-
-//         return collisionHandle;
-
-//     }
-
-// }
+        return inputDeps;
+    }
+}
