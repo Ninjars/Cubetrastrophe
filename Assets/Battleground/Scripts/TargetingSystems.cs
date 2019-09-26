5c365f88312bb568e01d972d2e41ff900d1e26e0
jez <jeremy+github@jeremystevens.net>
2019-09-26 22:32:00 +0100

    refactored targeting system to work with BurstCompiler; this required moving the component modification task out of the original job as Burst doesn't support CommandBuffer at this time.

--- Assets/Battleground/Scripts/TargetingSystems.cs
+++ Assets/Battleground/Scripts/TargetingSystems.cs
@@ -1,3 +1,4 @@
+using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
 using Unity.Jobs;
@@ -8,6 +9,7 @@ using UnityEngine;
 public class FindTargetJobSystem : JobComponentSystem {
     private EndSimulationEntityCommandBufferSystem endSimCommandBufferSystem;
     private EntityQuery targetQuery;
+    private EntityQuery searchingUnitQuery;
 
     private struct EntityWithPosition {
         public Entity entity;
@@ -16,12 +18,13 @@ public class FindTargetJobSystem : JobComponentSystem {
 
     [RequireComponentTag(typeof(DefenderTag))]
     [ExcludeComponent(typeof(HasTarget))]
+    [BurstCompile]
     private struct FindTargetJob : IJobForEachWithEntity<Translation> {
 
         [DeallocateOnJobCompletion]
         [ReadOnly]
         public NativeArray<EntityWithPosition> targetArray;
-        public EntityCommandBuffer.Concurrent commandBuffer;
+        public NativeArray<EntityWithPosition> closestTargetArray;
 
         public void Execute(Entity entity, int index, [ReadOnly] ref Translation translation) {
             Entity closest = Entity.Null;
@@ -29,7 +32,8 @@ public class FindTargetJobSystem : JobComponentSystem {
             float3 matchedTargetPosition = float3.zero;
             float bestDistance = float.MaxValue;
 
-            foreach (var item in targetArray) {
+            for (int i = 0; i < targetArray.Length; i++) {
+                var item = targetArray[i];
                 var targetEntity = item.entity;
                 var targetPosition = item.position;
                 var distance = math.distancesq(unitPosition, targetPosition);
@@ -48,10 +52,25 @@ public class FindTargetJobSystem : JobComponentSystem {
                 }
             }
 
-            if (closest != Entity.Null) {
+            closestTargetArray[index] = new EntityWithPosition { entity = closest, position = matchedTargetPosition };
+        }
+    }
+
+    [RequireComponentTag(typeof(DefenderTag))]
+    [ExcludeComponent(typeof(HasTarget))]
+    private struct AssignTargetJob : IJobForEachWithEntity<Translation> {
+
+        [DeallocateOnJobCompletion]
+        [ReadOnly]
+        public NativeArray<EntityWithPosition> closestTargetArray;
+        public EntityCommandBuffer.Concurrent commandBuffer;
+
+        public void Execute(Entity entity, int index, [ReadOnly] ref Translation translation) {
+            var target = closestTargetArray[index];
+            if (target.entity != Entity.Null) {
                 commandBuffer.AddComponent(index, entity, new HasTarget {
-                    targetEntity = closest,
-                    targetPosition = matchedTargetPosition,
+                    targetEntity = target.entity,
+                    targetPosition = target.position,
                     refreshTargetPeriod = 2f,
                     elapsedRefreshTime = 0f,
                 });
@@ -62,6 +81,7 @@ public class FindTargetJobSystem : JobComponentSystem {
     protected override void OnCreate() {
         endSimCommandBufferSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
         targetQuery = GetEntityQuery(typeof(TargetTag), ComponentType.ReadOnly<Translation>());
+        searchingUnitQuery = GetEntityQuery(typeof(DefenderTag), ComponentType.Exclude<HasTarget>());
     }
 
     protected override JobHandle OnUpdate(JobHandle inputDeps) {
@@ -78,12 +98,23 @@ public class FindTargetJobSystem : JobComponentSystem {
 
         targetEntities.Dispose();
         targetTranslations.Dispose();
-        var job = new FindTargetJob {
+
+        var closestTargetArray = new NativeArray<EntityWithPosition>(searchingUnitQuery.CalculateEntityCount(), Allocator.TempJob);
+        var findTargetJob = new FindTargetJob {
             targetArray = targetArray,
+            closestTargetArray = closestTargetArray,
+        };
+
+        // This separation is necessary because command buffers can't be used in Burst compiled structs
+        var assignTargetJob = new AssignTargetJob {
+            closestTargetArray = closestTargetArray,
             commandBuffer = endSimCommandBufferSystem.CreateCommandBuffer().ToConcurrent(),
         };
 
-        var jobHandle = job.Schedule(this, inputDeps);
+        // this makes the "find target" job a prerequisit for the "assign target" job
+        var jobHandle = findTargetJob.Schedule(this, inputDeps);
+        jobHandle = assignTargetJob.Schedule(this, jobHandle);
+
         endSimCommandBufferSystem.AddJobHandleForProducer(jobHandle);
         return jobHandle;
     }
