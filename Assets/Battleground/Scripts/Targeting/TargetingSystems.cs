414ad42d73d2b90c25e4e6b1a3965bace6b554c1
jez <jeremy+github@jeremystevens.net>
2019-11-09 17:42:13 +0000

    removed old notes from TargetingSystem

--- Assets/Battleground/Scripts/Targeting/TargetingSystems.cs
+++ Assets/Battleground/Scripts/Targeting/TargetingSystems.cs
@@ -6,7 +6,6 @@ using Unity.Mathematics;
 using Unity.Transforms;
 using UnityEngine;
 
-// developed with reference to Code Monkey at https://www.youtube.com/watch?v=nuxTq0AQAyY
 static class TargetingSystemUtils {
     public struct EntityWithPosition {
         public Entity entity;
@@ -259,60 +258,3 @@ public class RefreshHasTargetSystem : JobComponentSystem {
         return jobHandle;
     }
 }
-
-/*
-// kept for comparison
-public class HasTargetRefresh : ComponentSystem {
-    protected override void OnUpdate() {
-        var time = Time.deltaTime;
-        Entities.ForEach((Entity entity, ref Translation translation, ref HasTarget hasTarget) => {
-            hasTarget.elapsedRefreshTime += time;
-            if (hasTarget.elapsedRefreshTime > hasTarget.refreshTargetPeriod) {
-                PostUpdateCommands.RemoveComponent<HasTarget>(entity);
-            } else {
-                hasTarget.targetPosition = World.Active.EntityManager.GetComponentData<Translation>(hasTarget.targetEntity).Value;
-            }
-        });
-    }
-}
-*/
-
-/*
-// kept for comparison
-public class NearestTargetSystem : ComponentSystem {
-    protected override void OnUpdate() {
-        Entities.WithNone<HasTarget>().WithAll<DefenderTag>().ForEach((Entity entity, ref Translation entityTranslation) => {
-            Entity closest = Entity.Null;
-            float3 unitPosition = entityTranslation.Value;
-            float3 targetPosition = float3.zero;
-            float bestDistance = float.MaxValue;
-
-            Entities.WithAll<TargetTag>().ForEach((Entity targetEntity, ref Translation targetTranslation) => {
-                var distance = math.distancesq(unitPosition, targetPosition);
-
-                if (closest == null) {
-                    closest = targetEntity;
-                    bestDistance = distance;
-                    targetPosition = targetTranslation.Value;
-
-                } else {
-                    if (distance < bestDistance) {
-                        closest = targetEntity;
-                        bestDistance = distance;
-                        targetPosition = targetTranslation.Value;
-                    }
-                }
-            });
-
-            if (closest != Entity.Null) {
-                PostUpdateCommands.AddComponent(entity, new HasTarget {
-                    targetEntity = closest,
-                    targetPosition = targetPosition,
-                    refreshTargetPeriod = 2f,
-                    elapsedRefreshTime = 0f,
-                });
-            }
-        });
-    }
-}
-*/
